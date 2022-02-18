23d1eb727b3dc2eb29311a9704ebc52aca229f24
khalidhussain1 <khalidparacha0@gmail.com>
2022-02-18 11:43:57 +0500

    2323

--- app/Models/cites.php
+++ app/Models/cites.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class cites extends Model
 {
+    
+protected $fillable = [
+    'name',
+
+];
     use HasFactory;
 }
--- routes/web.php
+++ routes/web.php
@@ -39,32 +39,31 @@ Route::view('/faqs', 'frontend.faqs')->name('faqs1');
 Route::view('/log_in', 'frontend.login')->name('login');
 
 
-Route::get('/get/register', function() {
+Route::get('/get/register', function () {
 
     return view('frontend.register', [
-            'citiy' =>cites::all()
-        ]);
+        'citiy' => cites::all()
+    ]);
 });
 
 
 
-Route::prefix('/admins')->middleware(['auth','admin'])->group(function (){
+Route::prefix('/admins')->middleware(['auth', 'admin'])->group(function () {
 
-// admin panel routes 
-Route::view('/index', 'backend.admin.index')->name('index');
-Route::view('/referrals', 'backend.admin.sent-refferals')->name('referrals');
-Route::view('/settings', 'backend.admin.profile-settings')->name('settings');
-Route::view('/change-password', 'backend.admin.change-password')->name('changepassword');
-Route::view('/users', 'backend.admin.total-users')->name('users');
-Route::get('/agents', [admin::class, 'agents'])->name('agents');
-Route::get('/cities', [admin::class, 'cities'])->name('cities');
-Route::post('/add/cities', [admin::class, 'add_city']);
-Route::post('/cities/update/{id}', [admin::class, 'update_city']);
-Route::get('/cities/delete/{id}', [admin::class, 'delete_city']);
-Route::get('/approve/agents/{id}', [admin::class, 'approve']);
-Route::get('/reject/agents/{id}', [admin::class, 'reject']);
-Route::post('/update/{id}', [admin::class, 'update']);
-
+    // admin panel routes 
+    Route::view('/index', 'backend.admin.index')->name('index');
+    Route::view('/referrals', 'backend.admin.sent-refferals')->name('referrals');
+    Route::view('/settings', 'backend.admin.profile-settings')->name('settings');
+    Route::view('/change-password', 'backend.admin.change-password')->name('changepassword');
+    Route::view('/users', 'backend.admin.total-users')->name('users');
+    Route::get('/agents', [admin::class, 'agents'])->name('agents');
+    Route::get('/cities', [admin::class, 'cities'])->name('cities');
+    Route::post('/add/cities', [admin::class, 'add_city']);
+    Route::post('/cities/update/{id}', [admin::class, 'update_city']);
+    Route::get('/cities/delete/{id}', [admin::class, 'delete_city']);
+    Route::get('/approve/agents/{id}', [admin::class, 'approve']);
+    Route::get('/reject/agents/{id}', [admin::class, 'reject']);
+    Route::post('/update/{id}', [admin::class, 'update']);
 });
 
 
@@ -73,20 +72,17 @@ Route::post('/update/{id}', [admin::class, 'update']);
 
 // Agent panel routes 
 
-Route::prefix('/user')->middleware(['auth','user'])->group(function (){
-                        Route::view('/index', 'backend.agent.index')->name('index1');
-                        Route::get('/referrals1', [usercontroller::class, 'referrals'])->name('referrals1');
-Route::post('add/referral', [ReferralController::class, 'add_referral']);
+Route::prefix('/user')->middleware(['auth', 'user'])->group(function () {
+    Route::view('/index', 'backend.agent.index')->name('index1');
+    Route::get('/referrals1', [usercontroller::class, 'referrals'])->name('referrals1');
+    Route::post('add/referral', [ReferralController::class, 'add_referral']);
 
-Route::view('/settings1', 'backend.agent.profile-settings')->name('settings1');
-Route::view('/change-password1', 'backend.agent.change-password')->name('changepassword1');
-Route::get('/network', [ReferralController::class, 'network'])->name('network');
+    Route::view('/settings1', 'backend.agent.profile-settings')->name('settings1');
+    Route::view('/change-password1', 'backend.agent.change-password')->name('changepassword1');
+    Route::get('/network', [ReferralController::class, 'network'])->name('network');
 
-                Route::post('/personal-information', [agentController::class, 'update_information']);
-                Route::post('/change-password', [agentController::class, 'change_password']);
-
-
-
+    Route::post('/personal-information', [agentController::class, 'update_information']);
+    Route::post('/change-password', [agentController::class, 'change_password']);
 });
 
 
